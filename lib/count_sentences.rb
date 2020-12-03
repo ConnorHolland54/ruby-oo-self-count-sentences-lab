15d3620f2438519db52315acd4f9ec3237c5bf93
Connor Holland <supersoccergoalie123@gmail.com>
2020-12-03 12:21:02 -0700

    commit tree 3c3e9b09f44f36884622518cc2bd2b4ea7fc3cfb

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    count = 0
+    array = self.split(" ")
+    array.each do |word|
+      if word.include?(".") || word.include?("?") || word.include?("!")
+        count += 1
+      end
+    end
+
+    return count
 
   end
 end
